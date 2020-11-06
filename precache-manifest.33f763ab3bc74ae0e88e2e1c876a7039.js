5fbed3acce7d52ded0613714cafc6e72904ac72a
Lefter Alex <sasa.lefter@gmail.com>
2020-11-06 22:11:31 +0200

    Updates

---
+++ precache-manifest.33f763ab3bc74ae0e88e2e1c876a7039.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deae32b9bed7c4a0d187c2147ca75aee",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "1c38acba547847e841c4",
+    "url": "/react-recipe-app/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b83906c862f8eb26fccf",
+    "url": "/react-recipe-app/static/css/main.8e2159fe.chunk.css"
+  },
+  {
+    "revision": "1c38acba547847e841c4",
+    "url": "/react-recipe-app/static/js/2.3f564dc2.chunk.js"
+  },
+  {
+    "revision": "78fd0f62364854e39c53711d027f73a3",
+    "url": "/react-recipe-app/static/js/2.3f564dc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b83906c862f8eb26fccf",
+    "url": "/react-recipe-app/static/js/main.62203418.chunk.js"
+  },
+  {
+    "revision": "244229dd543193c8fac1",
+    "url": "/react-recipe-app/static/js/runtime-main.1ad87cc7.js"
+  }
+]);
